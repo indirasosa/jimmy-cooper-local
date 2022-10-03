5434aba4915ae95434ca577d0fb9b807229c4ad8
Indira Sosa <indirasosapellegrini@gmail.com>
2022-10-02 22:54:00 -0300

    poner icono carrito intento 1

---
+++ src/componentes/CartWidget.jsx
@@ -0,0 +1,7 @@
+import Image from 'react-bootstrap/Image'
+
+export const CartWidget = () => {
+    return (
+        <Image src='./img/carrito-icono.png'/>
+    )
+}
--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -2,12 +2,13 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import {CartWidget} from "./CartWidget";
 
 
 export const NavBar = () => {
     return (
-        <Navbar bg="light" expand="lg">
         <Container>
+        <Navbar bg="light" expand="lg">
           <Navbar.Brand href="#home">Jimmy Cooper
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -24,7 +25,8 @@ export const NavBar = () => {
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
-        </Container>
-      </Navbar>
+          <CartWidget/>
+        </Navbar>
+      </Container>
     )
 }
