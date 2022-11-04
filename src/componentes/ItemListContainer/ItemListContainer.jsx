685449b4e2be482166bb630e5594e52271b08948
Indira Sosa <indirasosapellegrini@gmail.com>
2022-11-04 14:38:57 -0300

    llamo a los productos filtrados por categoría

--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,29 +1,43 @@
 import {ItemList} from "../ItemList/ItemList";
 import { useState, useEffect } from "react"; 
-import {collection, doc, getDoc, getDocs} from "firebase/firestore";
+import {collection, doc, getDoc, getDocs, query, where} from "firebase/firestore";
 import { db } from "../../utils/firebase";
-
+import { useParams } from "react-router-dom";
 
 export const ItemListContainer = () => { 
 
+    const categoryName = useParams().categoryName;
     const [catalogo, setCatalogo] = useState([])
+
 
     useEffect(()=>{
         const getProductos = async()=>{
-            const queryRef = collection(db, "items");
-            const response = await getDocs(queryRef);
-            const documents = response.docs;
-            console.log(documents)
-            const productos = documents.map(elemento=>{
-                return({
-                    ...elemento.data(),
-                    id: elemento.id
+            if(categoryName){
+                const queryRef = query(collection(db, "items"), where("category", "==", categoryName));
+                const response = await getDocs(queryRef);
+                const documents = response.docs;
+                const productos = documents.map(elemento=>{
+                    return({
+                        ...elemento.data(),
+                        id: elemento.id
+                    })
                 })
-            })
-            setCatalogo(productos)
+                setCatalogo(productos)
+            } else {
+                const queryRef = collection(db, "items");
+                const response = await getDocs(queryRef);
+                const documents = response.docs;
+                const productos = documents.map(elemento=>{
+                    return({
+                        ...elemento.data(),
+                        id: elemento.id
+                    })
+                })
+                setCatalogo(productos)
+            }
         }
         getProductos();
-    },[])
+    },[categoryName])
     
     return (
     <div className="d-flex row m-4 center">
